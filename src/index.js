35c1c6e3dbf4c17ec985b8c13fc91381c1aa4637
atlassubbed <atlassubbed@gmail.com>
2019-03-24 03:42:55 -0400

    shortens comments and adds codesandbox support

--- src/index.js
+++ src/index.js
@@ -2,30 +2,26 @@ import { diff } from "atlas-relax";
 import { t } from "atlas-relax-jsx-pragmas";
 import DOMRenderer from "atlas-mini-dom";
 
+// this line is needed for codesandbox
+// (they do not support custom pragmas; see .babelrc's "pragma")
+window.React = { createElement: t }
+
 const minInterval = 0, maxInterval = 5e3;
 // force the number to be in [minInterval, maxInterval]
 const sanitize = v => Math.min(Math.max(Number(v), minInterval), maxInterval);
 const doPeriodicPollJob = () => console.log("we're polling something!");
 
 const PollingApp = (temp, node) => {
-  // initialize polling interval and count state on first render
-  if (node.count == null) node.count = 0;
-  if (node.period == null) node.period = 1e3;
-  // queue up next render after period has elapsed (hence periodic job)
-  node.diff(node.period)
-  // increment the job count number
-  node.count++;
-  // do some job
-  doPeriodicPollJob();
-  // event handler
+  if (node.count == null) node.count = 0; // init count
+  if (node.period == null) node.period = 1e3; // init period
+  node.diff(node.period) // queue next poll job (i.e. render)
+  node.count++; // we're rendering, increment the poll job count
+  doPeriodicPollJob(); // do actual job
   const updateInterval = event => {
-    // update the polling interval
-    node.period = sanitize(event.target.value);
-    // immediately queue up another poll job 
-    node.diff(0);
+    node.period = sanitize(event.target.value); // update poll interval
+    node.diff(0); // queue up next poll job immediately
   }
-  // return dom (could use fragments like <>...</>!)
-  return [
+  return [ // return DOM (could use fragments <>...</> if desired)
     <p>
       Polled {node.count} times
     </p>,
